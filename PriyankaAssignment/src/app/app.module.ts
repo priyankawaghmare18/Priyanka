35d931dc8483f9a4d689343b4acc47bdf7cce7b9
priyankawaghmaremore <63039867+priyankawaghmaremore@users.noreply.github.com>
2020-04-03 12:37:20 +0530

    Add files via upload

---
+++ PriyankaAssignment/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterUserComponent } from './register-user/register-user.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
